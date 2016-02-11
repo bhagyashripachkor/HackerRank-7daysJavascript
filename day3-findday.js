a081514d92f6448670fc30b9706b19329fd0efae
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-02-11 23:49:59 +0530

    Update day3-findday.js

--- day3-findday.js
+++ day3-findday.js
@@ -1,9 +1,11 @@
 function findDay(myDate) {
     // Return day for date myDate(MM/DD/YYYY)
     // Note that myDate contains the date in string format
-    var day = new Date(myDate); 
-    var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"] 
-    console.log(dayNames[day.getDay()]);
+    if(myDate != -1){
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+         var d = new Date(myDate);
+        console.log(days[d.getDay()]);
+    }
 } 
 
 // tail starts here
