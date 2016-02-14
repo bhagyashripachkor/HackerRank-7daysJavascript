cb3c7a234659cbcbd1d619f5a628d4c8d5f838cb
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-02-14 20:57:32 +0530

    Create day6-marktoys.js

---
+++ day6-marktoys.js
@@ -0,0 +1,24 @@
+function processData(input) {
+    //Enter your code here
+    var rows = input.split('\n');
+    var max = rows[0].split(' ').map(Number)[1];
+    var array = rows[1].split(' ').map(Number);
+    array = array.sort(function(a, b){return a-b}); 
+    var i = 0;
+    while(max>=0) {
+        max -= array[i];
+        i++;
+    }
+    console.log(i-1);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
